1eb75a8cb46251761dda3a05d6504dd232e3ec8c
pandeemiC <lkooxer55@hotmail.com>
2025-08-04 18:54:33 +0300

    flower pattern hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Floral from "@/components/ui/floral";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Floral />
         <Navbar />
         <main>{children}</main>
       </body>
---
+++ components/ui/floral.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import testFloral from "@/public/tool/testfloral.png";
+
+const Floral = () => {
+  return (
+    <div className="fixed inset-0 z-10 pointer-events-none">
+      <Image
+        src={testFloral}
+        width={120}
+        height={120}
+        alt="floral pattern"
+        className="absolute bottom-0 left-[-24px]"
+      />
+
+      <Image
+        src={testFloral}
+        width={120}
+        height={120}
+        alt="floral pattern"
+        className="absolute right-0 rotate-180 translate-x-1/4"
+      />
+    </div>
+  );
+};
+
+export default Floral;
