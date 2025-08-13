6b8153c06fc2c44db84a794278fa9b4c568c6121
pandeemiC <lkooxer55@hotmail.com>
2025-08-13 14:18:27 +0300

    true routes for main and sub categories for pagination

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,50 +1,28 @@
-// src/lib/routes.ts
+import { navigationData, type Category } from "@/components/ui/navigationData";
 
-export const ROUTES = {
-  HOME: "/",
+function toCamelCase(title: string) {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9]+(.)/g, (_, chr) => chr.toUpperCase());
+}
 
-  WOMAN: {
-    newArrivals: "/woman-new-arrivals",
-    bestSellers: "/woman-best-sellers",
-    jackets: "/woman-jackets",
-    dresses: "/woman-dresses",
-    tops: "/woman-tops",
-    bottoms: "/woman-bottoms",
+export const ROUTES = (Object.keys(navigationData) as Category[]).reduce(
+  (acc, category) => {
+    const { basePath, links } = navigationData[category];
+
+    acc[category] = {
+      all: basePath,
+    };
+
+    links.forEach((link) => {
+      if (link.href === "") {
+        return;
+      }
+
+      const key = toCamelCase(link.title);
+      acc[category][key] = `${basePath}/${link.href}`;
+    });
+    return acc;
   },
-
-  MAN: {
-    newArrivals: "/man-new-arrivals",
-    bestSellers: "/man-best-sellers",
-    jackets: "/man-jackets",
-    linen: "/man-linen",
-    tops: "/man-tops",
-    bottoms: "/man-bottoms",
-  },
-
-  KIDS: {
-    newArrivals: "/kids-new-arrivals",
-    bestSellers: "/kids-best-sellers",
-    girl: "/kids-g",
-    boy: "/kids-b",
-  },
-
-  HOME_CATEGORY: {
-    newArrivals: "/home-new-arrivals",
-    bestSellers: "/home-best-sellers",
-    bedding: "/home-bedding",
-    decor: "/home-decor",
-    furniture: "/home-furniture",
-    kitchen: "/home-kitchen",
-  },
-
-  FRAGRANCE: {
-    newArrivals: "/fragrance-new-arrivals",
-    bestSellers: "/fragrance-best-sellers",
-    forHer: "/fragrance-for-her",
-    forHim: "/fragrance-for-him",
-    unisex: "/fragrance-unisex",
-    giftSets: "/fragrance-gift-sets",
-  },
-} as const;
-
-export type RouteCategory = keyof typeof ROUTES;
+  {} as Record<Category, Record<string, string>>
+);
