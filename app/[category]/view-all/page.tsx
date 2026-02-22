671f2f7c9deb2986f2899b93f3d7b0e22b2d99e8
pandeemiC <lkooxer55@hotmail.com>
2026-02-22 13:23:30 +0300

    implemented a base for view all products page

--- app/[category]/view-all/page.tsx
+++ app/[category]/view-all/page.tsx
@@ -1,7 +1,47 @@
-export default function ViewAll() {
+import { getCategoryBySlug, getAllProductsWithImages } from "@/lib/queries";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function ViewAll({
+  params,
+}: {
+  params: Promise<{ category: string }>;
+}) {
+  const { category } = await params;
+  const categoryData = await getCategoryBySlug(category);
+
+  if (!categoryData) {
+    return <div>Category Not Found</div>;
+  }
+
+  const products = (await getAllProductsWithImages(categoryData.id)) ?? [];
+
   return (
-    <div>
-      <h1>View All</h1>
-    </div>
+    <main className="overflow-x-hidden">
+      <section className="px-53 py-20">
+        <h1 className="text-[13px] font-light uppercase tracking-widest mb-20 pl-1">
+          View All
+        </h1>
+        <div className="grid grid-cols-4 gap-5">
+          {products.map((product) => (
+            <Link key={product.id} href={`/${category}/view-all/${product.id}`}>
+              <div className="flex flex-col flex-wrap">
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  width={300}
+                  height={400}
+                  className="object-cover cursor-pointer"
+                />
+                <div className="product-label cursor-pointer">
+                  <h2 className="product-title">{product.name}</h2>
+                  <p className="product-price">${product.price}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
