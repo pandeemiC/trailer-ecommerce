135aad2e9ca38b1372d54dda497b0434703b8a9b
pandeemiC <lkooxer55@hotmail.com>
2026-02-25 15:49:58 +0300

    finished login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,12 @@ import { useState } from "react";
 import { createClient } from "@/lib/supabase/client";
 import { useRouter } from "next/navigation";
 import trailerLogo from "@/public/icons/trailer-logo-light.png";
+import authImg from "@/public/auth/authimg.jpg";
 import Link from "next/link";
 import Image from "next/image";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -84,7 +88,50 @@ export default function LoginPage() {
               {loading ? "Logging in..." : "Log In"}
             </button>
           </form>
+
+          <div className="flex items-center gap-4 my-8">
+            <hr className="flex-1 border-gray-300" />
+            <span className="text-[10px] text-gray-400 uppercase tracking-wider">
+              or
+            </span>
+            <hr className="flex-1 border-gray-300" />
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <button
+              onClick={() => handleOAuth("google")}
+              className="flex items-center justify-center gap-3 w-full border border-black py-3 text-[11px] tracking-[0.2em] uppercase hover:bg-black hover:text-white transition-colors"
+            >
+              <FcGoogle size={16} />
+              Continue with Google
+            </button>
+            <button
+              onClick={() => handleOAuth("github")}
+              className="flex items-center justify-center gap-3 w-full border border-black py-3 text-[11px] tracking-[0.2em] uppercase hover:bg-black hover:text-white transition-colors"
+            >
+              <FaGithub size={16} />
+              Continue with Github
+            </button>
+          </div>
+
+          <p className="text-center mt-12 text-[11px] tracking-wider text-gray-500">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="underline">
+              Sign Up
+            </Link>
+          </p>
         </div>
+      </div>
+
+      {/* right */}
+      <div className="w-1/2 relative">
+        <Image
+          src={authImg}
+          alt="Trailer Image"
+          fill
+          className="object-cover"
+          priority
+        />
       </div>
     </div>
   );
